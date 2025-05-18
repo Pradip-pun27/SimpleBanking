edcd1aa4d290f226e1fda91c3170d6b9ca7d2ff6
Pradip (Elon) mgr <pradippoonmgr@gmail.com>
2025-05-18 17:57:15 +0545

    Update random_pw_gen.py

--- random_pw_gen.py
+++ random_pw_gen.py
@@ -1,9 +1,13 @@
-# Random secure Pw generator python's program
-import string
+# Random secure P.W generator python's program
 import random
 letters = [chr(i) for i in range(ord('A'), ord('Z')+1)] + [chr(i) for i in range(ord('a'), ord('z')+1)]
 numbers =['0','1','2','3','4','5','6','7','8','9']
-symbols=list(string.punctuation) # list of special characters
+special_chars = [
+    '!', '@', '#', '$', '%', '^', '&', '*',
+    '(', ')', '-', '_', '=', '+', '[', ']',
+    '{', '}', ':', ';', ',', '.', '<', '>',
+    '?', '/', '|', '~'
+]
 print("Wlcm to the Pw generator Program")
 n_letter=int(input("How many letters u wanna put in yrs pw(Enter that number): "))
 n_number=int(input( "How many numbers u wanna put in yrs pw(Enter that number): "))
